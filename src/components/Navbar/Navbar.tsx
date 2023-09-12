1f12467df02214d2539bc11ad3f8b4f3cd80db6c
bots <botsone@gmail.com>
2023-09-11 20:50:52 -0600

    chore: added more deps, changed project structure, added assets

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Flex, Image } from "@chakra-ui/react"
+import React from "react"
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" alt="Reddit face logo"/>
+        <Image src="/images/redditText.svg" height="46px" alt="Reddit text logo"/>
+      </Flex>
+    </Flex>
+  )
+}
+export default Navbar
