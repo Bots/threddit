f4d63af4d39f67ce6f6e053fd392c6f30863ccc9
bots <botsone@gmail.com>
2023-09-11 21:36:12 -0600

    chore: added search input component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,30 @@
 import { Flex, Image } from "@chakra-ui/react"
 import React from "react"
+import SearchInput from "./SearchInput"
 
 const Navbar: React.FC = () => {
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+    >
       <Flex align="center">
-        <Image src="/images/redditFace.svg" height="30px" alt="Reddit face logo"/>
-        <Image src="/images/redditText.svg" height="46px" alt="Reddit text logo"/>
+        <Image
+          src="/images/redditFace.svg"
+          height="30px"
+          alt="Reddit face logo"
+        />
+        <Image
+          display={{ base: "none", md: "unset" }}
+          src="/images/redditText.svg"
+          height="46px"
+          alt="Reddit text logo"
+        />
       </Flex>
+      <SearchInput />
+      {/* <Directory />
+      <RightContent /> */}
     </Flex>
   )
 }
